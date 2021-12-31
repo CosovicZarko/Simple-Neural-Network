9560707fb960a6db67e5063d8ebeeb7b8eb5236e
CosovicZarko <93178157+CosovicZarko@users.noreply.github.com>
2021-12-30 19:04:15 -0800

    Update Simple_Neural_Network.py

--- Simple_Neural_Network.py
+++ Simple_Neural_Network.py
@@ -5,7 +5,7 @@ from keras.datasets import mnist
 # Loading the data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()                                                     
 x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]**2, 1)/255                      
-x_test = x_test.reshape (x_test.shape[0], x_test.shape[1]**2, 1)/255   
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1]**2, 1)/255   
 y_train = np_utils.to_categorical(y_train).reshape(y_train.shape[0], 10, 1) 
 y_test = np_utils.to_categorical(y_test).reshape(y_test.shape[0], 10, 1)
 
